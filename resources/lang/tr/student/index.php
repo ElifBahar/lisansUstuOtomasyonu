23358d1a514845d2be48f17fa85a80b8584cd811
Elif Bahar <elif.har94@gmail.com>
2022-04-15 23:29:54 +0300

    student index page için Türkçe ve İngilizce dil dosyası eklendi, Türkçe dosyanın içeriği dolduruldu #2

---
+++ resources/lang/tr/student/index.php
@@ -0,0 +1,77 @@
+<?php
+
+return [
+    'home_page' => 'ANA SAYFA',
+
+    'progress_header' => 'İlerlemeni Takip Et',
+    'progress_red_info' => 'KIRMIZI daireler henüz tamamlamadığınız adımları gösterir.',
+    'progress_yellow_info' => 'SARI daire şu anda üzerinde bulunduğunuz adımı gösterir.',
+    'progress_green_info' => 'YEŞİL daireler tamamladığınız adımları gösterir.',
+    'progress_step_one' => 'Kayıt',
+    'progress_step_two' => 'Bilgileriniz Formu Sayfa 1 (Kişisel Bilgiler)',
+    'progress_step_three' => 'Bilgileriniz Formu Sayfa 2 (Eğitim Bilgileri)',
+    'progress_step_four' => 'Bilgileriniz Formu Sayfa 3 (Dokümanlar)',
+    'progress_step_five' => 'Bilgileriniz Formu Sayfa 4 (Referans Bilgileri)',
+    'progress_step_six' => 'Bilgileriniz Formu Sayfa 5 (Sorular)',
+    'progress_step_seven' => 'İlana Başvuru',
+    'progress_step_eight' => 'Sonuçlar',
+
+    'application_status' => 'BAŞVURUNUZUN DURUMU',
+    'applications_status' => 'BAŞVURULARINIZIN DURUMU',
+    'applied_announcement' => 'Başvurduğunuz İlan',
+    'applied_announcement_waiting' => 'Bilgilerinizi ve dosyalarınızı kontrol ediyoruz',
+    'applied_announcement_approved' => 'Tebrikler kabul edildiniz, kabul maili size gönderilecektir.',
+    'applied_announcement_rejected' => 'Üzgünüz, bu ilana yaptığınız başvuru reddedildi.',
+    'applied_announcement_approved_mail' => 'Tebrikler kabul edildiniz. Kabul maili tarafınıza gönderilmiştir.',
+
+    'total_super_admin' => 'TOPLAM SÜPER ADMIN',
+    'general_statistics' => 'GENEL İSTATİSTİKLER',
+    'total_active_institute' => 'TOPLAM AKTİF ENSTİTÜ',
+    'total_students_all_institue' => 'TOPLAM ÖĞRENCİ (TÜM ENSTİTÜLER)',
+    'total_applied_students' => 'TOPLAM BAŞVURAN ÖĞRENCİ(TÜM ENSTİTÜLER)',
+    'institute_statistics' => 'ENSTİTÜ İSTATİSTİKLERİ',
+    'total_candidate_student' => 'TOPLAM ADAY ÖĞRENCİ',
+    'total_applied_student' => 'İLANA BAŞVURAN ÖĞRENCİ SAYISI',
+
+
+    'applied_student_nations' => 'Başvuran Öğrencilerin Milliyetleri',
+    'master' => 'Yüksek Lisans',
+    'nationality' => 'Milliyet',
+    'number_of_students' => 'Öğrenci Sayısı',
+    'doctorate' => 'Doktora',
+
+    'warning' => 'UYARI !',
+    'warning_header' => 'Başvurunuz için izlemeniz gereken adımlar:',
+    'first' => 'İlk olarak',
+    'first_explanation' => 'istenen tüm bilgileri sağlamalısınız. Cevaplamanız gereken 4 sayfa form bulunmaktadır ve bunları doğru şekilde tamamlarsanız son formu doldurduktan sonra',
+    'success' => 'Başarılı!',
+    'first_explanation_last' => 'uyarısı veren bir pop up görmelisiniz.',
+    'second' => 'İkinci olarak',
+    'second_explanation' => 'sizden istenen sayıda referansın mailinize onay vermesini beklemelisiniz. Referans onay durumunu bu sayfanın alt kısmında görebilirsiniz.',
+    'third' => 'Üçüncü olarak',
+    'third_explanation' => 'birinci ve ikinci aşamaları tamamladıktan sonra, lütfen yan menüdeki "Başvurular" sayfasına gidin ve sizin için doğru olan ilana başvuru yapın.',
+    'third_strong' => 'AŞAMALARI TAMAMLADIKTAN SONRA LÜTFEN BİR İLANA BAŞVURU YAPMAYI UNUTMAYIN.',
+    'third_explanation_last' => 'Sistem sizin için otomatik olarak başvuru yapmayacaktır!',
+    'last' => 'Sonuncu olarak',
+    'last_explanation' => 'bir duyuruya başvuru yaptıktan sonra, başvurunuzun durumunu ana sayfadan takip edebilirsiniz.',
+
+    'referencer_status' => 'Referans Durumu',
+    'referencer_status_small_one' => 'Aşağıda referans emailinizin durumunu görebilirsiniz:',
+    'referencer_red' => 'Kırmızı',
+    'referencer_declined' => 'referansınızın mailinize red cevabı verdiği veya henüz mail göndermediğiniz anlamına gelir.',
+    'referencer_yellow' => 'Sarı',
+    'referencer_waiting' => 'referans mailinizin gönderildiğini ancak referansınızın henüz dönüş yapmadığı anlamına gelir.',
+    'referencer_blue' => 'Mavi',
+    'referencer_approved' => 'referansınızın mailinizi onayladığı anlamına gelir.',
+    'referencer_name' => 'Referansın Adı',
+    'referencer_university_name' => 'Referansın Üniversite Adı',
+    'referencer_mail' => 'Referansın E-Maili',
+    'referencer_mail_declined_explanation' => 'Reddedildi veya Mail henüz gönderilmedi',
+    'referencer_mail_waiting_explanation' => 'Referanstan yanıt bekleniyor',
+    'referencer_mail_approved_explanation' => 'Onaylandı',
+    'update' => 'Güncelle',
+    'resend' => 'Maili Tekrar Gönder',
+
+
+
+];
