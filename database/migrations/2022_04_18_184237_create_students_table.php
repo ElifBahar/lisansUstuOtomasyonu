49729701a386670c61b2250b8afa3808749c4bc5
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-18 22:15:57 +0300

    students table migration #6

--- database/migrations/2022_04_18_184237_create_students_table.php
+++ database/migrations/2022_04_18_184237_create_students_table.php
@@ -15,6 +15,23 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name',100);
+            $table->string('surname',120);
+            $table->dateTime('birthday_date');
+            $table->string('mother_name',100);
+            $table->string('father_name',100);
+            $table->string('passport_no',11)->unique();
+            $table->string('tc_no',11)->nullable();
+            $table->string('nations',250);
+            $table->string('country',250);
+            $table->string('state',150)->nullable();
+            $table->string('province',150);
+            $table->text('phone');
+            $table->text('address');
+            $table->boolean('status')->comment('active 0 - incative 1')->default('0');
+            $table->tinyInteger('isDeleted')->comment('0 not deleted  - 1 deleted' )->default('0');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
