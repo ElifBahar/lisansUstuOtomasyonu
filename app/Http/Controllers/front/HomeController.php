c2fcacf2aa3737ab915a5435991400e521da4ff1
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-18 21:34:47 +0300

    add student landing page #5

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Announcements;
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $counterAnnouncement = Announcements::all() -> count();
+
+        $counterActiveAnnouncement = Announcements::where(['is_deleted' => 0,'status' => 1]) -> count();
+        $counterInActiveAnnouncement = Announcements::where(['is_deleted' => 0,'status' => 0]) -> count();
+
+        return view('student.index',compact('counterAnnouncement','counterActiveAnnouncement','counterInActiveAnnouncement'));
+    }
 }
