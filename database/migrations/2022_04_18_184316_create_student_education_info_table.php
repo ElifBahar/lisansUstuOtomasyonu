6bcdd82cb38b17337bba605ff041fd1dacd93106
Elif Bahar <elif.har94@gmail.com>
2022-04-18 21:45:00 +0300

    add student realted tables

---
+++ database/migrations/2022_04_18_184316_create_student_education_info_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentEducationInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_education_info', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_education_info');
+    }
+}
