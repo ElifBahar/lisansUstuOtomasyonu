00de09a92ad8fd66c6231c5fc92faabd4004ab4b
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-18 22:55:28 +0300

    student files info table migration #6

--- database/migrations/2022_04_18_184330_create_student_files_info_table.php
+++ database/migrations/2022_04_18_184330_create_student_files_info_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentEducationInfoTable extends Migration
+class CreateStudentFilesInfoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,37 +13,21 @@ class CreateStudentEducationInfoTable extends Migration
      */
     public function up()
     {
-        Schema::create('student_education_info', function (Blueprint $table) {
-
+        Schema::create('student_files_info', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('student_id');
-
-            $table->string('graduated_university', 255);
-            $table->string('graduated_university_country', 255);
-            $table->string('graduated_university_faculty', 255);
-            $table->string('graduated_university_department', 255);
-            $table->dateTime('graduated_date');
-
-            $table->boolean('is_master')->default(false);
-            $table->string('bachelor_degree_note_type', 20);
-            $table->string('master_degree_note_type', 20)->nullable();
-            $table->float('bachelor_degree_note_average', 7, 2);
-            $table->float('master_note_average', 5, 2)->nullable();
-
-            $table->string('master_graduated_university', 255)->nullable();
-            $table->string('master_graduated_university_country', 255)->nullable();
-            $table->string('master_graduated_university_faculty', 255)->nullable();
-            $table->string('master_graduated_university_department', 255)->nullable();
-            $table->dateTime('master_graduated_date')->nullable();
-
-            $table->string('language_exam', 255);
-            $table->tinyInteger('language_score');
-            $table->date('language_score_date');
-
-            $table->string('sufficiency_score', 255);
-            $table->integer('department_numerical_score');
-            $table->dateTime('sufficiency_score_date');
-            $table->string('request_department_of', 255);
+            $table->string('image',255);
+            $table->string('passport_file',255);
+            $table->string('identity_file',255);
+            $table->string('diploma_file',255);
+            $table->string('diploma_translate_file',255);
+            $table->string('transcript_file',255);
+            $table->string('transcript_translate_file',255);
+            $table->string('yds_file',255);
+            $table->string('sufficiency_file',255)->nullable();
+            $table->string('turkish_level_file_institution',150)->nullable();
+            $table->string('turkish_level_file')->nullable();
+            $table->string('language_level', 3);
             $table->index('student_id');
             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
             $table->timestamps();
@@ -57,6 +41,6 @@ class CreateStudentEducationInfoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('student_education_info');
+        Schema::dropIfExists('student_files_info');
     }
 }
