59219f956e043592cfb3d5e8c115bc01a97f79a6
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-23 00:06:03 +0300

    add Country model #8

---
+++ app/Models/Contry.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contry extends Model
+{
+    use HasFactory;
+    protected $table='countries';
+    protected $guarded = ['id'];
+    protected $fillable = ['name','description'];
+}
