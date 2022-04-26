00d61886f4b82f294d46684c3c391770f0949257
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-26 14:12:14 +0300

    add FenDepartmentsSeeder #9

---
+++ database/seeders/FenDepartmentsSeeder.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FenDepartmentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Su Ürünleri Yetiştiriciliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Mimarlık'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Otomotiv Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Biyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Biyoteknoloji (Disiplinler Arası)'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'İş ve Mühendislik Yönetimi (Disiplinler Arası)'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Kimya Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Kimya'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'İnşaat Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'İnşaat Mühendisliği Teknolojileri'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Bilgisayar Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Savunma Teknolojileri (Disiplinler Arası)'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Adli Bilişim Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Ekobilişim (Disiplinler Arası)'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Elektrik-Elektronik Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Enerji Sistemleri Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Çevre Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Su Ürünleri Temel Bilimler'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Su Ürünleri Avlama ve İşleme Teknolojisi'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Jeodezi ve Coğrafi Bilgi Teknolojileri (Disiplinler Arası)'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Jeoloji Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Matematik'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Makine Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Makine Mühendisliği Teknolojileri'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Mekatronik Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Metalurji ve Malzeme Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Metalurji ve Malzeme Mühendisliği Teknolojileri'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Fizik'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'Yazılım Mühendisliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 1,
+            'department_holder_id' => 1,
+            'department_name' => 'İstatistik'
+        ]);
+    }
+}
