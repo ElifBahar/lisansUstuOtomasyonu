4e133ba6485ca40e0b85ac7346496d8eadb9c458
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-26 14:09:08 +0300

    add SaglikDepartmentsSeeder #9

---
+++ database/seeders/SaglikDepartmentsSeeder.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SaglikDepartmentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 3,
+            'department_name' => 'Nöroendokrinoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Anatomi'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Biyofizik'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Biyoistatistik'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Tıbbi Biyokimya'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Enfeksiyon Hastalıkları'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Tıbbi Farmakoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Fizyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Halk Sağlığı'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Histoloji-Embriyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'İmmunoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Kulak Burun ve Boğaz Hastalıkları/Odyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Tıbbi Mikrobiyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Parazitoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 4,
+            'department_name' => 'Tıbbi Biyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Anatomi'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Besin Hijyeni ve Teknolojisi'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Biyokimya'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Cerrahi'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Doğum ve Jinekoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Dölerme ve Suni Tohumlama'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Farmakoloji ve Toksikoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Fizyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Hayvan Besleme ve Beslenme Hastalıkları'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Hayvan Sağlığı Ekonomisi ve İşletmeciliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Histoloji-Embriyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Mikrobiyoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Parazitoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Patoloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Veteriner Hekimliği Tarihi Deontoloji ve Etik'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Viroloji'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 5,
+            'department_name' => 'Zootekni'
+        ]);
+
+
+
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 6,
+            'department_name' => 'Beden Eğitimi ve Spor'
+        ]);
+
+
+
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 7,
+            'department_name' => 'Hemşirelik'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 7,
+            'department_name' => 'İç Hastalıkları Hemşireliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 7,
+            'department_name' => 'Halk Sağlığı Hemşireliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 7,
+            'department_name' => 'Çocuk Sağlığı ve Hastalıkları Hemşireliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 7,
+            'department_name' => 'Psikiyatri Hemşireliği'
+        ]);
+
+        DB::table('institute_departments')->insert([
+            'institute_id' => 3,
+            'department_holder_id' => 7,
+            'department_name' => 'Cerrahi Hastalıkları Hemşireliği'
+        ]);
+    }
+}
