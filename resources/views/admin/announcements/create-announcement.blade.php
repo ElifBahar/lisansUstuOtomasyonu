6e4e0e323afce0353cf944ad0659a4878bd4b66c
Elif Bahar <elif.har94@gmail.com>
2022-04-27 10:52:14 +0300

    update admin create-announcement file for view #10

--- resources/views/admin/announcements/create-announcement.blade.php
+++ resources/views/admin/announcements/create-announcement.blade.php
@@ -94,3 +94,176 @@
 
 
 </style>
+
+
+
+@section('content')
+
+
+
+    <form id="regForm" name="createAnnouncementForm" action="{{route('announcements-create')}}" method="POST">
+
+        @csrf
+        <h1>Announcement (İlan)</h1>
+        @if(isset($errors))
+            @if(count($errors)>0)
+                <div align="center" class="col-sm-12 alert alert-danger nomargin">
+                    @foreach($errors->all() as $error)
+                        {{$error}}<br>
+                    @endforeach
+                </div>
+        @endif
+    @endif
+    <!-- One "tab" for each step in the form: -->
+
+
+        <div class="row">
+            <div id="nameAlert" class="col-md-6 col-sm-12 announcement-alert alert alert-danger nomargin">Name must be filled out</div>
+            <div id="titleAlert" class="col-md-6 col-sm-12 announcement-alert alert alert-danger nomargin">Title must be filled out</div>
+            <div class="col-md-6 col-sm-12">
+                Name
+                <input id="name" value="{{old('name')}}" placeholder="Announcement Name (For admin panel)"  name="name"></p>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                Title
+                <input id="title" value="{{old('title')}}" placeholder="Title"  name="title">
+            </div>
+        </div>
+
+        <div class="row">
+            <br>
+            <div id="rDateAlert" class="col-md-6 col-sm-12 announcement-alert alert alert-danger nomargin">Entered release date is incorrect</div>
+            <div id="dDateAlert" class="col-md-6 col-sm-12 announcement-alert alert alert-danger nomargin">Entered due date is incorrect</div>
+            <div class="col-md-6 col-sm-12">
+                Release Date<br>
+                <input id="rDate" value="{{old('release_date')}}" class="dateMask" placeholder="Ex: 2020-02-31 14:41" type="datetime-local" id="release_date" name="release_date">
+            </div>
+            <div class="col-md-6 col-sm-12">
+                Due Date<br>
+                <input id="dDate" value="{{old('due_date')}}" class="dateMask" placeholder="Ex: 2020-02-31 14:41" type="datetime-local" id="due_date" name="due_date">
+            </div>
+        </div>
+        <div class="row">
+            <br>
+            <div id="contentAlert" class="col-xs-12 announcement-alert alert alert-danger nomargin">Content must be filled out</div>
+            <div class="col-xs-12">
+                <br>Content<br>
+                <textarea id="content" class="col-xs-12" rows="10" placeholder="Content"  name="content">{{old('content')}}</textarea></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <select class="col-xs-12" name="type">
+                    <option value="{{\App\Helpers\Enums::DOCTORATE_ENGLISH}}">Doctorate English</option>
+                    <option value="{{\App\Helpers\Enums::DOCTORATE_TURKISH}}">Doctorate Turkish</option>
+                    <option value="{{\App\Helpers\Enums::GRADUATE_ENGLISH}}">Graduate English</option>
+                    <option value="{{\App\Helpers\Enums::GRADUATE_TURKISH}}">Graduate Turkish</option>
+                </select></p>
+            </div>
+        </div>
+
+
+
+
+
+        <div style="overflow:auto;">
+            <div style="margin-top:20px;float:right;">
+                <button type="submit" id="submitButton">Submit</button>
+            </div>
+        </div>
+
+    </form>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
+    <script>
+
+        function validateForm() {
+            var name = document.getElementById("name").value;
+            var title = document.getElementById("title").value;
+            var institute = document.getElementById("institute")
+            var rDate = document.getElementById("rDate").value;
+            var dDate = document.getElementById("dDate").value;
+            var content = document.getElementById("content").value;
+            if (name == "") {
+                document.getElementById("nameAlert").style.visibility = "visible";
+                return false;
+            }else if(title == ""){
+                document.getElementById("titleAlert").style.visibility = "visible";
+                return false;
+            }else if(institute == ""){
+                document.getElementById("instituteAlert").style.visibility = "visible";
+            }else if(rDate == "" || rDate.substring(4,5)!="-" || rDate.substring(7,8)!="-" || rDate.substring(13,14)!=":"){
+                document.getElementById("rDateAlert").style.visibility = "visible";
+                return false;
+            }else if(dDate == "" || dDate.substring(4,5)!="-" || dDate.substring(7,8)!="-" || dDate.substring(13,14)!=":"){
+                document.getElementById("dDateAlert").style.visibility = "visible";
+                return false;
+            }else if(content == ""){
+                document.getElementById("content").style.visibility = "visible";
+                return false;
+            }
+        }
+    </script>
+
+    <script type="text/javascript" src="{{asset('/plugins/ckeditor5/build/ckeditor.js')}}"></script>
+
+    <script>
+        ClassicEditor
+            .create( document.querySelector( 'textarea#content' ), {
+
+                toolbar: {
+                    items: [
+                        'heading',
+                        '|',
+                        'bold',
+                        'italic',
+                        'link',
+                        'bulletedList',
+                        'numberedList',
+                        '|',
+                        'indent',
+                        'outdent',
+                        '|',
+                        'blockQuote',
+                        'insertTable',
+                        'mediaEmbed',
+                        'undo',
+                        'redo'
+                    ]
+                },
+                language: 'en',
+                image: {
+                    toolbar: [
+                        'imageTextAlternative',
+                        'imageStyle:full',
+                        'imageStyle:side'
+                    ]
+                },
+                table: {
+                    contentToolbar: [
+                        'tableColumn',
+                        'tableRow',
+                        'mergeTableCells'
+                    ]
+                },
+                licenseKey: '',
+
+            } )
+            .then( editor => {
+                editor.getData(); // -> '<p>Foo!</p>'
+            } )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+
+
+
+
+
+
+@endsection
+
