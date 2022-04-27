c91d2e80a9f1b7d32fdd3a79e50e4a22165780cc
Elif Bahar <elif.har94@gmail.com>
2022-04-27 10:48:25 +0300

    add countries table #7

---
+++ database/migrations/2022_04_27_074748_create_countries_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
